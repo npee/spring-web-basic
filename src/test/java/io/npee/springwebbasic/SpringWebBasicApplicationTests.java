9d0b085f4ca9d91f51b70e9cf4d8aebac2be2286
npee <jmc4664@gmail.com>
2021-10-02 17:59:19 +0900

    Initial Commit

---
+++ src/test/java/io/npee/springwebbasic/SpringWebBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.npee.springwebbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
