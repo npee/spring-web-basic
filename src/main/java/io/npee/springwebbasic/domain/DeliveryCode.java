0a6ba23a85ecb98ec95d7ad5b8a6ed8671b1b84f
npee <jmc4664@gmail.com>
2021-10-17 02:40:49 +0900

    입력 폼 처리 - 요구사항 추가 - 판매 여부, 등록 지역, 상품 종류, 배송 방식

---
+++ src/main/java/io/npee/springwebbasic/domain/DeliveryCode.java
@@ -0,0 +1,11 @@
+package io.npee.springwebbasic.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeliveryCode {
+    private String code;
+    private String displayName;
+}
