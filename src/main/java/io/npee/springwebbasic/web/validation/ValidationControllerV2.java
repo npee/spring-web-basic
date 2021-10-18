b2d2be59202d4f2dd246f3fd2badcf88635ce388
npee <jmc4664@gmail.com>
2021-10-19 04:22:22 +0900

    검증 - Validation v2 - Validator 사용하여 검증 코드 분리

--- src/main/java/io/npee/springwebbasic/web/validation/ValidationControllerV2.java
+++ src/main/java/io/npee/springwebbasic/web/validation/ValidationControllerV2.java
@@ -24,6 +24,7 @@ import java.util.*;
 public class ValidationControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @ModelAttribute("regions")
     public Map<String, String> regions() {
@@ -224,7 +225,7 @@ public class ValidationControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item,
                             BindingResult bindingResult,
                             RedirectAttributes redirectAttributes,
@@ -249,6 +250,29 @@ public class ValidationControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        // 생략 시 @ModelAttribute 자동 적용
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item,
+                            BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes,
+                            Model model) {
+
+        itemValidator.validate(item, bindingResult);
 
         // 검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
