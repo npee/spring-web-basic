6a32dd34205708d139e01221594e4833fbce9e36
npee <jmc4664@gmail.com>
2021-10-20 23:29:46 +0900

    로그인 - 직접 만든 세션을 사용한 로그인

--- src/main/java/io/npee/springwebbasic/web/login/LoginController.java
+++ src/main/java/io/npee/springwebbasic/web/login/LoginController.java
@@ -2,6 +2,7 @@ package io.npee.springwebbasic.web.login;
 
 import io.npee.springwebbasic.domain.login.LoginService;
 import io.npee.springwebbasic.domain.member.Member;
+import io.npee.springwebbasic.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
@@ -20,13 +22,14 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController {
 
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
+    // @PostMapping("/login")
     public String login(@Validated @ModelAttribute("loginForm") LoginForm form,
                         BindingResult bindingResult,
                         HttpServletResponse response) {
@@ -49,9 +52,37 @@ public class LoginController {
 
     }
 
-    @PostMapping("/logout")
+    @PostMapping("/login")
+    public String loginV2(@Validated @ModelAttribute("loginForm") LoginForm form,
+                        BindingResult bindingResult,
+                        HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다. ");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리(세션 쿠키)
+        sessionManager.createSession(loginMember, response);
+
+        return "redirect:/";
+
+    }
+
+    // @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
         expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request) {
+        sessionManager.expire(request);
         return "redirect:/";
     }
 
