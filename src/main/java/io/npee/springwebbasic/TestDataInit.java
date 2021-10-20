a41f4c3cad6dbc6b950a9993203f553dba8971fc
npee <jmc4664@gmail.com>
2021-10-20 17:35:42 +0900

    로그인 - 회원가입 기능

--- src/main/java/io/npee/springwebbasic/TestDataInit.java
+++ src/main/java/io/npee/springwebbasic/TestDataInit.java
@@ -2,6 +2,8 @@ package io.npee.springwebbasic;
 
 import io.npee.springwebbasic.domain.item.Item;
 import io.npee.springwebbasic.domain.item.ItemRepository;
+import io.npee.springwebbasic.domain.member.Member;
+import io.npee.springwebbasic.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,18 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setName("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+        memberRepository.save(member);
+
     }
 }
