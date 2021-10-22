d76737053ffadb7285118a58b0bed1ffd3cef11f
npee <jmc4664@gmail.com>
2021-10-22 21:04:56 +0900

    타입 컨버터 - 타임리프에서 컨버터 사용하기

---
+++ src/main/java/io/npee/springwebbasic/web/test/ConverterController.java
@@ -0,0 +1,46 @@
+package io.npee.springwebbasic.web.test;
+
+import io.npee.springwebbasic.web.converter.IpPort;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/test")
+public class ConverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converter/converter-view";
+    }
+
+    @GetMapping("/converter/edit")
+    public String converterForm(Model model) {
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        Form form = new Form(ipPort);
+        model.addAttribute("form", form);
+        return "converter/converter-form";
+    }
+
+    @PostMapping("/converter/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+        return "converter/converter-view";
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Form {
+        private IpPort ipPort;
+    }
+}
