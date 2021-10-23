efe931472717477917a1b34055ded6f418dee4c1
npee <jmc4664@gmail.com>
2021-10-23 22:10:21 +0900

    파일 업로드 - 파일 업로드 예제

---
+++ src/main/java/io/npee/springwebbasic/domain/item/ItemForm.java
@@ -0,0 +1,15 @@
+package io.npee.springwebbasic.domain.item;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class ItemForm {
+
+    private Long itemId;
+    private String itemName;
+    private List<MultipartFile> imageFiles;
+    private MultipartFile attachFile;
+}
